27421c6bf5c157cc56acd05b7c2612e9bf0d882a
ransomar3 <ransomar3@vcu.edu>
2017-09-12 02:34:50 -0400

    Added starting statement asking for user input values and if statement (incomplete so far)

--- INFO450MakeChange/INFO450MakeChange.cpp
+++ INFO450MakeChange/INFO450MakeChange.cpp
@@ -9,9 +9,18 @@ using namespace std;
 int main()
 {
 	// int variables to be included:
-	int money;
-	int result;
+	int cost, item;
+	int paid;
 	int dollar, quarter, dime, nickel; 
+
+	cout << "Please enter the cost of the item:\n" << endl;
+	cout << "Please enter the amount you are paying:\n" << endl;
+
+	if (paid >= item)
+	{
+		cout << "Your change is:" << endl;
+	}
+
 
     return 0;
 }
