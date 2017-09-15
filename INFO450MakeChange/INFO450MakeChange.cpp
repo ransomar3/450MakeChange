b93e448e16e61f72794273784f37b402cfe55920
ransomar3 <ransomar3@vcu.edu>
2017-09-14 23:58:05 -0400

    paid > cost now displays # of change given back

--- INFO450MakeChange/INFO450MakeChange.cpp
+++ INFO450MakeChange/INFO450MakeChange.cpp
@@ -12,7 +12,7 @@ int main()
 	// bool included at the end of program to ask user if they want to continue the loop or not.
 	// doubles declared for accuracy in change.
 	double cost, paid, paid2, change = 1;
-	double dollar = 1.00 , quarter = 0.25, dime = 0.10, nickel = 0.05;
+	double cents = 1.00;
 	bool resume = true;
 	char answer;
 
@@ -21,43 +21,42 @@ int main()
 	cout << "You entered: " << cost << endl;
 	cout << "Please enter the amount you are paying:\n " << endl;
 	cin >> paid;
-	cout << "You entered: " << paid << endl;
-
-	while (paid > cost)
-	{
-		// while loop giving you the amount of change you're receiving if the sufficient funds are provided. 
-		cout << "Thank you. Your change is:\n " << change << endl;
-		cout << "\n# Dollars: " << dollar << endl;
-		cout << "\n# Quarters: " << quarter << endl;
-		cout << "\n# Dimes: " << dime << endl;
-		cout << "\n# Nickels: " << nickel << endl;
-		dollar = change / 100;
-		quarter = change / 0.25;
-		dime = change / 0.10;
-		nickel = change / 0.05;
-
-	}
+	cout << "You entered:\n " << paid << endl;
 
 	while (paid < cost)
 	{
 		// while loop asking for more amounts if insufficient funds are provided for the cost of the item.
-		cout << "That's not enough money! You need " << cost-paid << " more." << endl;
-		cout << "Please enter in the additional amount:\n " << endl;
+		cout << "That's not enough money! You need $ " << cost-paid << " more." << endl;
+		cout << "Please enter in the additional amount: $ " << endl;
 		cin >> paid2;
 		paid = paid + paid2;
 		
+		if (paid > cost)
+		{
+			// while loop giving you the amount of change you're receiving if the sufficient funds are provided. 
+			change = paid - cost;
+			cout << "Thank you. Your change is: " << "$ " << change << endl;
+			cents = change * 100;
+			cout << "# Dollars: " << cents << endl;
+			change = change/0.25;
+			cout << "# Quarters: " << cents << endl;
+			cents = change/0.10;
+			cout << "# Dimes: " << cents << endl;
+			cents = change/0.05;
+			cout << "# Nickels: " << cents << endl;
+
+		}
 	
 		cout << "Would you like to enter another item? Press '1' to continue or '0' to exit. " << endl;
 		cin >> answer;
-		if (answer = '1')
+		if (answer == '1')
 		{
 			continue;	
 		}
-		else if (answer = '0')
+		else if (answer == '0')
 		{
 			resume = false;
 		}
-		cin.ignore();
 	}
 	system("Pause");
     return 0;
